4e89b97e045e51cb8e432c3c0fcafeec75b4f9b0
RenanPizzettiSilvano <renanpsilvano@gmail.com>
2017-06-04 21:54:04 -0300

    Funcoes de servico para informacoes pessoais e de medico.

--- js/Services/usuarioService.js
+++ js/Services/usuarioService.js
@@ -6,6 +6,7 @@ const URI_REST_RECUPERAR_SENHA = `${RestService.webService}/`;
 const UsuarioService = {
     cadastrarUsuario: cadastrarUsuario,
     recuperarSenha: recuperarSenha,
+    salvarInformacoesPessoais: salvarInformacoesPessoais,
     salvarMedico: salvarMedico
 };
 
@@ -17,8 +18,12 @@ function recuperarSenha(body) {
     return RestService.post(URI_REST_RECUPERAR_SENHA, body);
 }
 
-function salvarMedico(user,body) {
-    return RestService.post(`${URI_REST_USUARIO}/${user}/medicos`,body);
+function salvarInformacoesPessoais(body) {
+    return RestService.post(`${URI_REST_USUARIO}/`, body);
+}
+
+function salvarMedico(user, body) {
+    return RestService.post(`${URI_REST_USUARIO}/${user}/medicos`, body);
 }
 
 export default UsuarioService;
