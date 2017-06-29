24ff5a20f93513ab5ee4863ec41feb9c98542355
Rafael Marangoni Waterkemper <rafaelwaterkemper@gmail.com>
2017-06-29 12:34:43 -0300

    Criado o método put.

--- js/Services/restService.js
+++ js/Services/restService.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const RestService = {
-    webService: 'http://192.168.19.2:3000/api/v1',
+    webService: 'http://192.168.43.222:3000/api/v1',
     get: get,
     post: post,
-    patch: patch
+    patch: patch,
+    put: put
 };
 
 const headers = {
-    'Accept': 'application/json',
+    'Accept': 'application/json
     'Content-Type': 'application/json'
 };
 
@@ -23,6 +24,15 @@ function post(uri, body) {
         body: JSON.stringify(body)
     }).then((response) => response.json());
 }
+
+function put(uri, body) {
+    return fetch(uri, {
+        method: 'PUT',
+        headers: headers,
+        body: JSON.stringify(body)
+    }).then((response) => response.json());
+}
+
 function patch(uri, body) {
     return fetch(uri, {
         method: 'PATCH',
